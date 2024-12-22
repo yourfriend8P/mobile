c0b57a1e2d4bfbfbc4a46bf5f7297a96e32dba27
yourfriend <prabalshakya11@gmail.com>
2024-12-22 13:41:43 +0545

    this is awesome

--- components/Food_Container.js
+++ components/Food_Container.js
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { StyleSheet } from "react-native";
 import FoodCounterButton from "./FoodCounterButton";
+import MyButton from "./MyButton";
 
 const Food_Container = ({ title, pts }) => {
   return (
@@ -9,10 +10,17 @@ const Food_Container = ({ title, pts }) => {
       <View style={styles.imgContainer}></View>
       <View style={styles.funcContainer}>
         <View style={styles.funcConainter_left}>
-          <Text style={{ fontSize: 16, fontWeight: "bold" }}>{title}</Text>
-          <Text>{pts} pts</Text>
+          <View style={styles.funcContainer_left_up}>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>{title}</Text>
+            <Text>{pts} pts</Text>
+          </View>
+          <View style={styles.funcContainer_left_down}>
+            <Text>Button here</Text>
+          </View>
         </View>
-        <View style={styles.funcConainter_right}></View>
+        <View style={styles.funcConainter_right}>
+          <MyButton></MyButton>
+        </View>
       </View>
     </View>
   );
@@ -44,8 +52,21 @@ const styles = StyleSheet.create({
     padding: 5,
     flexDirection: "row",
   },
-  funcConainter_left: { borderWidth: 1, flex: 1, padding: 10 },
-  funcConainter_right: { borderWidth: 1, flex: 1, padding: 10 },
+  funcConainter_left: { borderWidth: 1, flex: 1, padding: 4 },
+  funcConainter_right: {
+    borderWidth: 1,
+    padding: 4,
+    flex: 1,
+    padding: 10,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  funcContainer_left_up: { borderWidth: 1, flex: 1 },
+  funcContainer_left_down: {
+    borderWidth: 1,
+    flex: 1,
+    justifyContent: "flex-end",
+  },
 });
 
 export default Food_Container;
--- components/logo.js
+++ components/logo.js
@@ -1,12 +1,10 @@
 import { Image } from "react-native";
 import React from "react";
 
-const Logo = ({ value }) => {
+const Logo = () => {
   return (
     <Image
-      source={{
-        uri: ",
-      }} // Replace with your image URL
+      source={require("../assets/images/foodcourtlogo.png")} // Use require for local images
       style={{ width: 300, height: 300, marginTop: 20 }}
     />
   );
